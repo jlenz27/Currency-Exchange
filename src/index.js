1474592b6aea15f1b829a9d73643d84c280d89f2
John Lenz <john.lenz27@gmail.com>
2023-02-10 10:31:54 -0800

    added buisness logic and listeners

--- src/index.js
+++ src/index.js
@@ -2,3 +2,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './ExchangeService.js';
+
+
+//Business Logic
+
+function getExchange() {
+  let promise = ExchangeService.getExchange();
+  promise.then(function(exchangeRate) {
+    printElements(exchangeRate);
+  }, function(error) {
+    printError(error);
+  });
+}
+
+function printElements(currencyData) {
+
+}
+
+
+
+function printError(error) {
+  const errorType = Object.values(error[1]);
+  document.querySelector('#showResults').innerText = `There was an error: ${errorType[3]}`;
+}
+function handleFormSubmission(event) {
+  event.preventDefault();
+  getExchange();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
