a601b40b2bbad66df88ceea65a2d17b7e89adff0
John Lenz <john.lenz27@gmail.com>
2023-02-19 23:16:43 -0800

    fixed resubmission

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,64 @@
-// Business Logic
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+// import ExchangeService from './exchangeService.js';
 
-// Export example
-// export default function Example()
+// //Business Logic
 
+// function getExchange(amount, currency) {
+//   ExchangeService.getExchangeRate(currency)
+//     .then(function(currencyResponse) {
+//       if (currencyResponse instanceof Error) {
+//         const errorMessage = `there was a problem accessing the data from currency exchange request: ${currencyResponse.message}.`;
+//         throw new Error(errorMessage);
+//       }
+//       printElements(currency,amount);
+//     })
+//     .catch(function(error) {
+//       printError(error);
+//     });
+// }
+
+
+// //UI Logic
+
+// function printElements(currencyData, amount, currency) {
+//   const exchangeRate = currencyData[0]["conversion_rates"][currency];
+//   if (currencyData[0]["conversion_rates"][currency] === undefined) {
+//     document.querySelector('#showResults').innerText = "We are unable to find the exchange rate at this time make sure you enter a valid number";
+//   } else {
+//     document.querySelector('#showResults').innerText = `$${amount} USD = ${(exchangeRate * amount).toFixed(2)} ${currency}`;
+//   }
+// }
+
+// function printError(error) {
+//   document.querySelector('#showResults').innerText = error;
+// }
+// function handleFormSubmission(event) {
+//   event.preventDefault();
+//   const amount = document.querySelector('#amount').value;
+//   const currency = document.querySelector('#currency').value;
+//   getExchange(amount,currency);
+// }
+
+// window.addEventListener("load", function() {
+//   document.querySelector('form').addEventListener("submit", handleFormSubmission);
+// });
+
+
+export default class ExchangeService {  
+    static async getExchangeRate() {
+      try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+        const jsonifiedResponse = await response.json();
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}
+          ${jsonifiedResponse.message}`;
+          throw new Error(errorMessage);
+        }
+        return jsonifiedResponse;
+      } catch(error) {
+        return error;
+      }
+    }
+  } 
