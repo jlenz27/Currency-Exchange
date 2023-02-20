e16f847b4dafa97fa4d9dd770c06b464a29b2b6c
John Lenz <john.lenz27@gmail.com>
2023-02-20 09:09:03 -0800

    added license

--- src/js/main.js
+++ src/js/main.js
@@ -46,19 +46,19 @@
 // });
 
 
-export default class ExchangeService {  
-    static async getExchangeRate() {
-      try {
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-        const jsonifiedResponse = await response.json();
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}
-          ${jsonifiedResponse.message}`;
-          throw new Error(errorMessage);
-        }
-        return jsonifiedResponse;
-      } catch(error) {
-        return error;
-      }
-    }
-  } 
+// export default class ExchangeService {  
+//     static async getExchangeRate() {
+//       try {
+//         const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+//         const jsonifiedResponse = await response.json();
+//         if (!response.ok) {
+//           const errorMessage = `${response.status} ${response.statusText}
+//           ${jsonifiedResponse.message}`;
+//           throw new Error(errorMessage);
+//         }
+//         return jsonifiedResponse;
+//       } catch(error) {
+//         return error;
+//       }
+//     }
+//   } 
