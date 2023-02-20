1f812b94f19c27b8fd7b4a753d80cf6f58a6dc68
John Lenz <john.lenz27@gmail.com>
2023-02-19 23:18:05 -0800

    Update index.js
    updated to pass test

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ function getExchange() {
   let promise = ExchangeService.getExchange();
   promise.then(function(exchangeRate) {
     printElements(exchangeRate);
-  }, function(error) {
+  }).catch(function(error){
     printError(error);
   });
 }
-
 
 
 //UI Logic
@@ -22,7 +21,7 @@ function printElements(currencyData) {
   const amount = document.querySelector('#amount').value;
   const currency = document.querySelector('#currency').value;
   const exchangeRate = currencyData[0]["conversion_rates"][currency];
-  if (currencyData[0]["conversion_rates"][currency] === "undefined") {
+  if (currencyData[0]["conversion_rates"][currency] === undefined) {
     document.querySelector('#showResults').innerText = "We are unable to find the exchange rate at this time make sure you enter a valid number";
   } else {
     document.querySelector('#showResults').innerText = `$${amount} USD = ${(exchangeRate * amount).toFixed(2)} ${currency}`;
@@ -30,9 +29,18 @@ function printElements(currencyData) {
 }
 
 function printError(error) {
-  const errorType = Object.values(error[1]);
-  document.querySelector('#showResults').innerText = `There was an error: ${errorType[3]}`;
+  if (error[0].status === 404) {
+    document.querySelector("#showResults").innerText = `API Endpoint not found`;
+  } else if (error[0].status === 403) {
+    document.querySelector("#showResults").innerText = `API Invalid`;
+  } else {
+    const err = Object.values(error[1]);
+    const errorMessage = `There was an error in accessing data: ${err[3]}`;
+    document.querySelector("#showResults").innerText = errorMessage;
+  }
+  document.querySelector("#showResults").classList.add("error");
 }
+
 function handleFormSubmission(event) {
   event.preventDefault();
   getExchange();
